72ed9b18aeb8aa586fe8fa6656d46a788beac325
mHfL28A/MQmFC+XY88eunJLolHwchv3S+G3etcK5qOI= <xuedua070@cdh61-login3.bd.cluster.cesga.es>
2024-12-10 00:11:01 +0100

    a

--- Exercicio1.1/mapper.py
+++ Exercicio1.1/mapper.py
@@ -12,6 +12,6 @@ for line in sys.stdin:
     data = line.strip().split("\t")
    
     if len(data) != 6:
-
+       continue
     date, time, store, item, cost, payment = data
-    print(store+"\t"+cost) 
+    print(store+"\t"+cost)  
