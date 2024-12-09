dfff57acb2693397f2dbdfaa5a63607b5794a940
mHfL28A/MQmFC+XY88eunJLolHwchv3S+G3etcK5qOI= <xuedua070@cdh61-login3.bd.cluster.cesga.es>
2024-12-10 00:38:53 +0100

    a

---
+++ Exercicio1.5/mapper_max.py
@@ -0,0 +1,14 @@
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+
+    if len(data) != 6:
+        continue
+
+    try:
+        date, time, store, item, cost, payment = data
+        cost = float(cost)
+        print("{0}".format(cost))
+    except (ValueError, IndexError):
+        continue
---
+++ Exercicio1.5/reducer_max.py
@@ -0,0 +1,9 @@
+import sys
+
+ventas = 0
+
+for line in sys.stdin:
+    
+    ventas += 1
+
+print(ventas)
